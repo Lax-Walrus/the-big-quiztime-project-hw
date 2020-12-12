6567ab7691d442ca58a127d115b945e854a73ef3
Kevin Lyons <zinckev@hotmail.com>
2020-12-12 14:08:18 -0800

    commiting for skeleton of html, timers functional in js current project is questions and answers

---
+++ assests/javascript.js
@@ -0,0 +1,92 @@
+console.log("this is linked");
+var answerButtons = document.querySelector(".answer-area");
+var timer = document.querySelector("#timer-clock");
+var startQuizTimer = document.querySelector("#start-quiz-timer");
+var score = document.querySelector("#score");
+var question = document.querySelector("#question");
+var startButton = document.querySelector("#start-button");
+var questions = ["Question 1", "Question 2", "Question 3", "Question 4"];
+var question1A = ["answer 1", "answer 2", "answer 3", "answer 4"];
+var question2A = ["answer 21", "answer 22", "answer 23", "answer 24"];
+var question3A = ["answer 31", "answer 32", "answer 33", "answer 34"];
+var question4A = ["answer 41", "answer 42", "answer 43", "answer 44"];
+var quizTime = 99;
+var startingTime = 5;
+
+question.style.display = "none";
+
+startButton.addEventListener("click", function () {
+  var startTimerInterval = setInterval(function () {
+    startingTime--;
+    startQuizTimer.textContent = startingTime + " seconds left till pop quiz";
+    if (startingTime === 0) {
+      clearInterval(startTimerInterval);
+      startQuizTimer.textContent = "";
+      startButton.parentNode.removeChild(startButton);
+      startQuiz();
+    }
+  }, 1000);
+});
+
+function startQuiz() {
+  question.style.display = "block";
+
+  var timeInterval = setInterval(function () {
+    quizTime--;
+    timer.textContent = quizTime + " left in quiz";
+    if (quizTime === 0) {
+      clearInterval(quizTime);
+      hiscores();
+    }
+  }, 1000);
+
+  for (let i = 0; i < questions.length; i++) {
+    var ranQuestion1 = questions[i];
+    ranQuestion1[Math.floor(Math.random() * ranQuestion1 1)];
+    question.textContent = ranQuestion1;
+    console.log(ranQuestion1);
+
+    if (ranQuestion1 === "Question 1") {
+      for (let i = 0; i < question1A.length; i++) {
+        var ranAnswer1 = question1A[i];
+        ranAnswer1[Math.floor(Math.random() * ranAnswer1.length)];
+        var q1btns = document.createElement("button");
+        q1btns.textContent = ranAnswer1;
+
+        answerButtons.appendChild(q1btns);
+      }
+    }
+    if (ranQuestion1 === "Question 2") {
+      for (let i = 0; i < question2A.length; i++) {
+        var ranAnswer2 = question2A[i];
+        ranAnswer2[Math.floor(Math.random() * ranAnswer2.length)];
+        var q2btns = document.createElement("button");
+        q2btns.textContent = ranAnswer2;
+
+        answerButtons.appendChild(q2btns);
+      }
+    }
+    if (ranQuestion1 === "Question 3") {
+      for (let i = 0; i < question3A.length; i++) {
+        var ranAnswer3 = question3A[i];
+        ranAnswer3[Math.floor(Math.random() * ranAnswer3.length)];
+        var q3btns = document.createElement("button");
+        q3btns.textContent = ranAnswer3;
+
+        answerButtons.appendChild(q3btns);
+      }
+    }
+    if (ranQuestion1 === "Question 4") {
+      for (let i = 0; i < question4A.length; i++) {
+        var ranAnswer4 = question4A[i];
+        ranAnswer4[Math.floor(Math.random() * ranAnswer4.length)];
+        var q4btns = document.createElement("button");
+        q4btns.textContent = ranAnswer4;
+
+        answerButtons.appendChild(q4btns);
+      }
+    }
+  }
+
+  console.log(q1btns, q2btns, q3btns, q4btns);
+}
