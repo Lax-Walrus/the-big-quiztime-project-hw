90648afa25b6fbe74de172428bdf1109dbbc1bcc
Kevin Lyons <zinckev@hotmail.com>
2020-12-12 19:33:02 -0800

    reworking questions, shows and answer buttons show trying add eventlistener for correct button

--- assests/javascript.js
+++ assests/javascript.js
@@ -7,13 +7,14 @@ var question = document.querySelector("#question");
 var startButton = document.querySelector("#start-button");
 var scoreContainer = document.querySelector(".score-container");
 var nameInput = document.querySelector("#name-input");
+var grade = document.querySelector("#grade");
 var names = [];
-var questions = [1, 2, 3, 4];
+var questions = ["Question 1", "Question 2", "Question 3", "Question 4"];
 var question1A = ["answer 1", "answer 2", "answer 3", "answer 4"];
 var question2A = ["answer 21", "answer 22", "answer 23", "answer 24"];
 var question3A = ["answer 31", "answer 32", "answer 33", "answer 34"];
 var question4A = ["answer 41", "answer 42", "answer 43", "answer 44"];
-var quizTime = 1;
+var quizTime = 99;
 var startingTime = 5;
 var finalScore = 99;
 
@@ -27,7 +28,7 @@ startButton.addEventListener("click", function () {
     if (startingTime === 0) {
       clearInterval(startTimerInterval);
       startQuizTimer.textContent = "";
-      startButton.parentNode.removeChild(startButton);
+      startButton.style.display = "none";
       startQuiz();
     }
   }, 1000);
@@ -44,11 +45,34 @@ function startQuiz() {
       hiscores();
     }
   }, 1000);
-
-  function quizQuestions() {
-    questionOne.createElement("h1");
-    questionOne.textContent = "Question one goes here";
+  for (let i = 0; i < question.length; i++) {
+    var questionOne = questions[i];
   }
+  var questionOne = questions.slice(0, 1, 2);
+
+  var questionArea1 = document.createElement("h1");
+  questionArea1.textContent = questionOne;
+  question.appendChild(questionArea1);
+
+  for (let i = 0; i < question1A.length; i++) {
+    var answerOne = question1A[i];
+
+    var answersQuestionOne = document.createElement("button");
+    answersQuestionOne.textContent = answerOne;
+    answerButtons.appendChild(answersQuestionOne);
+  }
+  answerButtons.addEventListener("click", function () {
+    if ( === true) {
+      var correctanswerloc = document.createElement("h1");
+      correctanswerloc.textContent = "correct";
+      answerButtons.appendChild(correctanswerloc);
+    } else {
+      quizTime--;
+      var wronganswerloc = document.createElement("h1");
+      wronganswerloc.textContent = "wrong";
+      answerButtons.appendChild(wronganswerloc);
+    }
+  });
 }
 
 function hiscores() {
